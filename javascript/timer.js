32aecd530412538d7575aeeda63a6a71cde061ab
Ansh Chauhan <ansh.chauhan15apr@gmail.com>
2022-03-25 23:48:36 +0530

    timer UI updated

--- javascript/timer.js
+++ javascript/timer.js
@@ -1,14 +1,14 @@
-var start = document.getElementById("start");
-var reset = document.getElementById("reset");
+var start = document.querySelector("#starting");
+var reset = document.querySelector("#stop");
 
-var h = document.getElementById("hour");
-var m = document.getElementById("minute");
-var s = document.getElementById("sec");
+var h = document.querySelector("#hour");
+var m = document.querySelector("#minute");
+var s = document.querySelector("#sec");
 var startTimer = null;
-
 start.addEventListener("click", function () {
   if (h.value == 0 && m.value == 0 && s.value == 0) {
-    document.querySelector("#start").style.visibility = "visible";
+    document.querySelector("#starting").style.visibility =
+      "visible";
     alert("Set timer");
   } else {
     function startInterval() {
@@ -16,7 +16,7 @@ start.addEventListener("click", function () {
         timer();
       }, 1000);
     }
-    document.querySelector("#start").style.visibility = "hidden";
+    document.querySelector("#starting").style.visibility = "hidden";
     startInterval();
   }
 });
@@ -25,7 +25,7 @@ reset.addEventListener("click", function () {
   m.value = 0;
   s.value = 0;
   stopInterval();
-  document.querySelector("#start").style.visibility = "visible";
+  document.querySelector("#starting").style.visibility = "visible";
 });
 function timer() {
   if (h.value == 0 && m.value == 0 && s.value == 0) {
@@ -34,7 +34,7 @@ function timer() {
     s.value = 0;
     stopInterval();
     PlaySound();
-    document.querySelector("#start").style.visibility = "visible";
+    document.querySelector("#starting").style.visibility = "visible";
   } else if (s.value != 0) {
     s.value--;
   } else if (m.value != 0 && s.value == 0) {
@@ -47,9 +47,9 @@ function timer() {
   return;
 }
 function stopInterval() {
-  clearInterval(startTimer);
-}
-function PlaySound() {
-  var sound = new Audio("../Assets/audio/alert-new.mp3");
-  sound.play();
-}
+    clearInterval(startTimer);
+  }
+  function PlaySound() {
+    var sound = new Audio("../Assets/audio/alert-new.mp3");
+    sound.play();
+  }
