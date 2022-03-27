5fd6293d468e3917b6c23692c681574b53264c3e
Subhransu <subhranshumajhi2@gmail.com>
2022-03-27 23:27:24 +0530

    blog app completed

--- javascript/blog.js
+++ javascript/blog.js
@@ -1,27 +1,55 @@
+let blogContainer = document.querySelector(".blog-container");
+
+const medium_url = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40bitbybit_vitb";
 function displayBlog() {
-  const medium_url =
-    "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40bitbybit_vitb";
     fetch(medium_url).then(res=>{
         return res.json();
     }).then(data => {
-        for(var i=0;i<data.items.length;i++){
-            console.log(data.items[i].title);
+        let blogData = "";
+        data.items.forEach(element =>{
+            
+            let blog = `
+            <div class="blog-card container">
+                <div class="blog-img">
+                    <a href="${element["link"]}" target="-blank">
+                    <img src="${element["thumbnail"]}">
+                    </a>
+                </div>
+                <button class="blog-acc">${element["title"]}   <i class="fa-solid fa-caret-down d1">  </i></button>
+                <div class="blog-body">
+                    <p> ${element["description"]} </p>
+                </div>
+            </div>
+            <br>`;
+            
+            blogData += blog;
+        })
+        blogContainer.innerHTML = blogData;
+        
+        var acc = document.getElementsByClassName("blog-acc");
+        var i;
+        
+        for (i = 0; i < acc.length; i++) {
+          acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+              panel.style.display = "none";
+            } else {
+              panel.style.display = "block";
+            }
+          });
         }
+
     })
 }
 
 displayBlog();
 
 
-document.querySelector(".d1").addEventListener('click', function (){
-    document.querySelector(".hide-init1").classList.toggle("hide1");
-})
-document.querySelector(".d2").addEventListener('click', function (){
-    document.querySelector(".hide-init2").classList.toggle("hide2");
-})
-document.querySelector(".d3").addEventListener('click', function (){
-    document.querySelector(".hide-init3").classList.toggle("hide3");
-})
-document.querySelector(".d4").addEventListener('click', function (){
-    document.querySelector(".hide-init4").classList.toggle("hide4");
-})
+
+
+
+
+
+
